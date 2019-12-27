43e1acae25f534c8243ab1faf4f19e9ec66d77c4
Grzegorz Gajoch <gajoch@gmail.com>
2019-12-27 00:37:35 +0000

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 #![no_main]
 #![no_std]
 
-//use panic_semihosting as _;
 use panic_halt as _;
 use stm32f0xx_hal as hal;
 use crate::hal::{prelude::*};
 use stm32f0;
 use rtfm;
-use core::ptr::{write_volatile, read_volatile};
 
 
 /// Tries to write a byte to the UART
@@ -31,15 +29,12 @@ fn read(usart: *const hal::stm32::usart1::RegisterBlock) -> nb::Result<u8, hal::
     let isr = unsafe { (*usart).isr.read() };
 
     let err = if isr.pe().bit_is_set() {
-//        hprintln!("Parity");
         nb::Error::Other(hal::serial::Error::Parity)
     } else if isr.fe().bit_is_set() {
-//        hprintln!("Frame");
         nb::Error::Other(hal::serial::Error::Framing)
     } else if isr.nf().bit_is_set() {
         nb::Error::Other(hal::serial::Error::Noise)
     } else if isr.ore().bit_is_set() {
-//        hprintln!("Overrun");
         nb::Error::Other(hal::serial::Error::Overrun)
     } else if isr.rxne().bit_is_set() {
         // NOTE(read_volatile) see `write_volatile` below
@@ -65,53 +60,12 @@ fn read(usart: *const hal::stm32::usart1::RegisterBlock) -> nb::Result<u8, hal::
     Err(err)
 }
 
-fn dbg(text : &str) {
+fn print(text : &str) {
     for c in text.bytes() {
         nb::block!(write(stm32f0::stm32f0x1::USART1::ptr(), c)).unwrap();
     }
     nb::block!(write(stm32f0::stm32f0x1::USART1::ptr(), '\n' as u8)).unwrap();
 }
-fn dbgc(c : char) {
-    nb::block!(write(stm32f0::stm32f0x1::USART1::ptr(), c as u8)).unwrap();
-}
-
-
-fn set_13() {
-    unsafe { (*stm32f0::stm32f0x1::GPIOB::ptr()).odr.modify(|_, x| {
-        x.odr13().bit(true)
-    }); }
-}
-fn clear_13() {
-    unsafe { (*stm32f0::stm32f0x1::GPIOB::ptr()).odr.modify(|_, x| {
-        x.odr13().bit(false)
-    }); }
-}
-
-fn set_14() {
-    unsafe { (*stm32f0::stm32f0x1::GPIOB::ptr()).odr.modify(|_, x| {
-        x.odr14().bit(true)
-    }); }
-}
-fn clear_14() {
-    unsafe { (*stm32f0::stm32f0x1::GPIOB::ptr()).odr.modify(|_, x| {
-        x.odr14().bit(false)
-    }); }
-}
-
-fn set_15() {
-    unsafe { (*stm32f0::stm32f0x1::GPIOB::ptr()).odr.modify(|_, x| {
-        x.odr15().bit(true)
-    }); }
-}
-fn clear_15() {
-    unsafe { (*stm32f0::stm32f0x1::GPIOB::ptr()).odr.modify(|_, x| {
-        x.odr15().bit(false)
-    }); }
-
-}
-
-
-
 
 #[rtfm::app(device=crate::hal::stm32f0::stm32f0x1, peripherals = true)]
 const APP: () = {
@@ -121,16 +75,10 @@ const APP: () = {
         let mut rcc = p.RCC.configure().sysclk(8.mhz()).freeze(&mut p.FLASH);
 
         let gpioa = p.GPIOA.split(&mut rcc);
-        let gpiob = p.GPIOB.split(&mut rcc);
 
-
-
-        let (tx, rx, dbg1, dbg2, dbg3) = cortex_m::interrupt::free(|cs| {
+        let (tx, rx) = cortex_m::interrupt::free(|cs| {
             (gpioa.pa9.into_alternate_af1(cs),
-             gpioa.pa10.into_alternate_af1(cs),
-             gpiob.pb13.into_push_pull_output(cs),
-             gpiob.pb14.into_push_pull_output(cs),
-             gpiob.pb15.into_push_pull_output(cs),)
+             gpioa.pa10.into_alternate_af1(cs))
         });
 
 
@@ -139,43 +87,25 @@ const APP: () = {
         serial.listen(hal::serial::Event::Rxne);
     }
 
-    #[task(priority = 1)]
-    fn printx(_: printx::Context) {
-        set_15();
-//        nb::block!(write(stm32f0::stm32f0x1::USART1::ptr(), '?' as u8)).unwrap();
-        let mut x = 0_u32;
-        loop {
-            unsafe {
-                let now = read_volatile(&mut x);
-                write_volatile(&mut x, now+1);
-                if now > 100 {
-                    break;
-                }
-            }
-        }
-        clear_15();
+    #[task(priority = 1, capacity = 10)]
+    fn time_consuming_task(_: time_consuming_task::Context) {
+        print("time consuming task");
     }
 
-    #[task(priority = 2)]
-    fn printx2(_: printx2::Context) {
-    }
+//    #[task(priority = 2)]
+//    fn printx2(_: printx2::Context) {
+//    }
 
-    #[task(priority = 8, binds = USART1, spawn = [printx])]
-    fn USART1_handler(_cx: USART1_handler::Context) {
-        set_13();
-
-        if let Ok(c) = read(stm32f0::stm32f0x1::USART1::ptr()) {
-            if c == '\n' as u8 {
-                set_14();
-                _cx.spawn.printx().unwrap();
-                clear_14();
-                dbgc('X');
+    #[task(priority = 8, binds = USART1, spawn = [time_consuming_task])]
+    fn usart_handler(_cx: usart_handler::Context) {
+        match read(stm32f0::stm32f0x1::USART1::ptr()) {
+            Ok(_c) => {
+                _cx.spawn.time_consuming_task().unwrap();
             }
-        } else {
-            dbg("error!");
+            Err(_e) => {
+                print("error!");
+            }
         }
-
-        clear_13();
     }
 
     extern "C" {
